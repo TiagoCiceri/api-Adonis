3b397f17e198726be74b50cc90567de1ed466bef
TiagoCicieri <tiagocicei@gmail.com>
2020-10-27 19:29:39 -0300

    finalizando base api

--- app/Controllers/Http/TarefaController.js
+++ app/Controllers/Http/TarefaController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Tarefa = use('App/Models/Tarefa');
+const Database = use('Database');
 const { validadeAll } = use('Validator');
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
@@ -20,7 +21,18 @@ class TarefaController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request, response, view, auth }) {
+    //const tarefas = await Tarefa.all();
+    const tarefas = await Tarefa.query()
+                                //.select('titulo', 'descricao')
+                                .where('user_id', auth.user.id)
+                                .withCount('arquivos as total_arquivos')
+                                .fetch();
+    //const tarefas = Database.select('titulo', 'descricao')
+    //                       .from('tarefas')
+    //                       .where('user_id', auth.user.id);
+
+    return tarefas;
   }
 
     /**
@@ -54,7 +66,20 @@ class TarefaController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show ({ params, request, response, view, auth }) {
+    const tarefa = Tarefa.query()
+                         //.select('titulo', 'descricao')
+                         .where('user_id', auth.user.id)
+                         .where('id', '=', params.id)
+                         .first();
+
+    if (!tarefa) {
+      return response.status(401).send({message: 'Nenhum tarefa foi localizada.'});
+    }
+
+    //await tarefa.load('arquivos');
+
+    return tarefa;
   }
 
   /**
@@ -65,7 +90,25 @@ class TarefaController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params, request, response, auth }) {
+    const { titulo, descricao } = request.all();
+
+    const tarefa = await Tarefa.query()
+                               .where('user_id', auth.user.id)
+                               .where('id', '=', params.id)
+                               .first();
+          
+    if (!tarefa) {
+      return response.status(401).send({message: 'Nenhum registro localizado.'});
+    }
+
+    tarefa.titulo = titulo;
+    tarefa.descricao = descricao;
+    tarefa.id = params.id;
+
+    await tarefa.save();
+
+    return tarefa;
   }
 
   /**
@@ -76,8 +119,20 @@ class TarefaController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy ({ params, request, response, auth }) {
+    const tarefa = await Tarefa.query()
+                               .where('user_id', auth.user.id)
+                               .where('id','=',params.id)
+                               .first();
+
+    if (!tarefa) {
+      return response.status(404).send({message: 'Nenhum registro localizado.'});
+    }
+
+    await tarefa.delete();
+
+    return response.status(200).send({message: 'Registro removiso com sucesso!!!'});
+  }  
 }
 
 module.exports = TarefaController
--- start/routes.js
+++ start/routes.js
@@ -13,3 +13,5 @@ Route.post('/user', 'UserController.create');
 Route.post('/login', 'UserController.login');
 
 Route.resource('tarefa', 'TarefaController').apiOnly().middleware('auth');
+
+Route.post('/tarefa/:id/arquivo', 'ArquivoController.create').middleware('auth');
